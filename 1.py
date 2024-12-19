faee10d4d0d3b9ae599412d2d9142219941d779d
TatarnikovSergey <sg.tatarnikov@yandex.ru>
2024-12-19 22:23:16 +0500

    сделать скрипт для загрузки данных в БД

--- 1.py
+++ 1.py
@@ -1,3 +1,31 @@
-a = '12345'
+import pandas as pd
+import sqlite3
 
-print(a[-2:])
+# Шаг 1: Чтение данных из Excel файла
+excel_file = 'ваш_файл.xlsx'  # Укажите путь к вашему Excel файлу
+sheet_name = 'Лист1'  # Укажите имя листа, если необходимо
+data = pd.read_excel(excel_file, sheet_name=sheet_name)
+
+# Шаг 2: Создание (или подключение к) SQLite базы данных
+conn = sqlite3.connect('ваша_база_данных.db')  # Укажите имя вашей базы данных
+cursor = conn.cursor()
+
+# Шаг 3: Создание таблицы (если она еще не существует)
+# Предположим, что у вас есть колонки 'id', 'name', 'age' в Excel
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS ваша_таблица (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    age INTEGER
+)
+''')
+
+# Шаг 4: Заполнение таблицы данными из DataFrame
+data.to_sql('ваша_таблица', conn, if_exists='append', index=False)
+
+# Шаг 5: Закрытие соединения с базой данных
+conn.commit()
+conn.close()
+
+print("Данные успешно загружены в базу данных!")
+
--- date_time.py
+++ date_time.py
@@ -1,19 +1,24 @@
-# from time import time
-# import sys
 from datetime import datetime, date, timedelta, time
 from pyrogram import Client, filters
 from pyrogram.handlers import MessageHandler
+import sqlite3
+
+
+
 
 BIRTHDAY_DAD = datetime(1984, 5, 29)
 BIRTHDAY_MOM = datetime(1986, 1, 13)
 BIRTHDAY_LIZA = datetime(2015, 12, 21)
 BIRTHDAY_VARYA = datetime(2012, 4, 11)
 
-# current_day = date.today()
+
 current_day = datetime.now()
-# dad = datetime.combine(BIRTHDAY_DAD, time())
-# print(datetime.time(datetime.now()))
-# print(datetime.now())
+contacts = {'Varya': 2029375186, 'Me': 'me', 'N': 1840301878}
+api_id = 20144058
+api_hash = '438ee2de12ff65b320453dda15c4a37d'
+# Создаём программный клиент, передаём в него
+# имя сессии и данные для аутентификации в Client API
+app = Client('my_session', api_id=api_id, api_hash=api_hash)
 
 last_digit_days = ('2', '3', '4')
 last_digit_day = '1'
@@ -35,34 +40,40 @@ def get_declination(count_days):
 def calculate_days(birthday):
     if current_day.strftime("%m-%d") < birthday.strftime("%m-%d"):
         result = birthday.day-current_day.day
-        return f'День рождения через {result} {get_declination(result)}!' \
-               f' Исполнится {(current_day.year-birthday.year)} лет'
+        if result <= 3:
+            app.start()
+            app.send_message(contacts.get('Me'), f'День рождения через {result} {get_declination(result)}!')
+            app.stop()
+        # return f'День рождения через {result} {get_declination(result)}!' \
+        #        f' Исполнится {(current_day.year-birthday.year)} лет'
     elif current_day.strftime("%m-%d") > birthday.strftime("%m-%d"):
         # next_year = birthday.replace(int(date.today().year + 1))  # прибавляем год
         next_year = datetime.replace(birthday, year=int(current_day.year + 1))
         result = (next_year - current_day).days
-        # if result <= 3:
+        if result <= 3:
+            app.start()
+            app.send_message(contacts.get('Me'), f'День рождения через {result} {get_declination(result)}!'f' Исполнится {(next_year.year-birthday.year)} лет')
+            app.stop()
+               # f' Исполнится {(next_year.year-birthday.year)} лет'#{a if str((next_year - current_day).days)[-1] in tr else c if str((next_year - current_day).days)[-1] == tr1 else b}'
+    # else:
+    #     return 'Сегодня день рождения!'
 
-        return f'День рождения через {result} {get_declination(result)}!' \
-               f' Исполнится {(next_year.year-birthday.year)} лет'#{a if str((next_year - current_day).days)[-1] in tr else c if str((next_year - current_day).days)[-1] == tr1 else b}'
-    else:
-        return 'Сегодня день рождения!'
 
-
-print(calculate_days(BIRTHDAY_LIZA))
-print(calculate_days(BIRTHDAY_DAD))
-print(calculate_days(BIRTHDAY_MOM))
-print(calculate_days(BIRTHDAY_VARYA))
+# print(calculate_days(BIRTHDAY_LIZA))
+# print(calculate_days(BIRTHDAY_DAD))
+# print(calculate_days(BIRTHDAY_MOM))
+# print(calculate_days(BIRTHDAY_VARYA))
 # print(calculate_days(date(2020, 11, 10)))
 
 
 
 
 if __name__ == '__main__':
-    print('Программа расчета количества дней до дня рождения!')
-
-    birthday = input('Введите дату рождения в формате "день.месяц.год". '
-                     'Пример 5.12.1995 \nДля выхода введите "Q"  \n')
-    birthday = datetime.strptime(birthday, '%d.%m.%Y')
-    print(calculate_days(birthday))
+    # print('Программа расчета количества дней до дня рождения!')
+    #
+    # birthday = input('Введите дату рождения в формате "день.месяц.год". '
+    #                  'Пример 5.12.1995 \nДля выхода введите "Q"  \n')
+    # birthday = datetime.strptime(birthday, '%d.%m.%Y')
+    # print(calculate_days(birthday))
+    calculate_days(BIRTHDAY_LIZA)
 
